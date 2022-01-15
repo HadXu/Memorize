6a6b6d2a21e753a595ff4ad7cde103b99723e061
lei <hadxu123@gmail.com>
2022-01-15 11:37:38 +0800

    finish lesson 7

---
+++ Memorize/Cardify.swift
@@ -0,0 +1,36 @@
+//
+//  Cardify.swift
+//  Memorize
+//
+//  Created by lei on 2022/1/15.
+//
+
+import SwiftUI
+
+struct Cardify: ViewModifier {
+    var isFaceUp: Bool
+    
+    func body(content: Content) -> some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius:DrawingConstants.cornerRadius)
+            if isFaceUp  {
+                shape.fill().foregroundColor(.white)
+                shape.strokeBorder(lineWidth: DrawingConstants.linewidth)
+            } else {
+                 shape.fill()
+            }
+            content.opacity(isFaceUp ? 1 : 0)
+        }
+    }
+    
+    private struct DrawingConstants {
+        static let cornerRadius: CGFloat = 10
+        static let linewidth: CGFloat = 3
+    }
+}
+
+extension View {
+    func cardify(isFaceUp: Bool) -> some View {
+        self.modifier(Cardify(isFaceUp: isFaceUp))
+    }
+}
